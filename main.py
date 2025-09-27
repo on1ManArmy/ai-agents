34bd0ba9834848065a056ec90a65290ab16b3dc2
Abhishek Kumar <abhishek.kumar4@gds.ey.com>
2025-09-27 22:09:31 +0530

    code changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def main():
     print("Hello from langchain-course!")
-    print(os.getenv("OPENAI_API_KEY"))
+    API_KEY = os.getenv("OPENAI_API_KEY")
+    print("Api Key is: ", API_KEY)
 
 if __name__ == "__main__":
     main()
