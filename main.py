951d283097f8059926d31fea6f89c8c6b80b688e
Abhishek <iitjeeabhishek@gmail.com>
2026-05-07 14:20:46 +0530

    Added tavily search capability

--- main.py
+++ main.py
@@ -2,32 +2,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain.agents import create_agent
-from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_groq import ChatGroq
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
-tavily = TavilyClient()
-
-@tool
-def search(query: str) -> dict:
-    """
-    Tool that searches over internet
-    Args:
-        query: The search query for internet
-    Returns:
-        The search result
-    """
-    print("Searching for {}".format(query))
-    return tavily.search(query)
-
-llm = ChatGroq(model="llama-3.1-8b-instant")
-tools = [search]
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
     result = agent.invoke({"messages": HumanMessage(
-        content="What is weather in Hyderabad only today temprature?"
+        content="Give me 3 Job search options from linkedin regarding AWS, Nodejs, and AI agets"
     )})
     print(result["messages"][-1].content)
 
