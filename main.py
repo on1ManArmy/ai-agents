4c757614ba25cf4847bb2c4186a22ca37a48e525
Abhishek <iitjeeabhishek@gmail.com>
2026-05-07 14:09:39 +0530

    Search tool for temprature

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+from tavily import TavilyClient
+
+tavily = TavilyClient()
+
+@tool
+def search(query: str) -> dict:
+    """
+    Tool that searches over internet
+    Args:
+        query: The search query for internet
+    Returns:
+        The search result
+    """
+    print("Searching for {}".format(query))
+    return tavily.search(query)
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
 def main():
-    print("Hello from ai-agents!")
+    result = agent.invoke({"messages": HumanMessage(
+        content="What is weather in Hyderabad only today temprature?"
+    )})
+    print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     main()
